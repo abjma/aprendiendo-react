c0ec762011ff31fa095bcfb3abc9784fc561dcfa
Abel Jose <abelj.monts97@gmail.com>
2024-01-10 15:43:46 -0500

    Tic Tac Toe
    Desarrollo del juego 3 en raya

---
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -0,0 +1,82 @@
+import { useState } from 'react'
+import confetti from 'canvas-confetti'
+
+import { Square } from './components/Square'
+import { TURNS } from './constants'
+import { checkWinner } from './logic/board'
+import { WinnerModal } from './components/WinnerModal'
+
+function App() {
+  const [board, setBoard] = useState(
+    Array(9).fill(null)
+  )
+
+  const [turn, setTurn] = useState(TURNS.x)
+
+  const [winner, setWinner] = useState(null)
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.x)
+    setWinner(null)
+  }
+
+  const checkEndGame = (newBoard) => {
+
+    return newBoard.every((square) => square !== null)
+  }
+
+  const updateBoard = (index) => {
+    if (board[index] || winner) return
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.x ? TURNS.o : TURNS.x
+    setTurn(newTurn)
+
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      confetti()
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
+    }
+  }
+
+  return (
+
+    <main className='board'>
+      <h1>TIC TAC TOE</h1>
+      <button onClick={resetGame}>Reiniciar</button>
+      <section className="game">
+        {
+          board.map((square, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {square}
+              </Square>
+            )
+          })
+        }
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.x}>
+          {TURNS.x}
+        </Square>
+        <Square isSelected={turn === TURNS.o}>
+          {TURNS.o}
+        </Square>
+      </section>
+
+      <WinnerModal winner={winner} />
+    </main>
+
+  )
+}
+
+export default App
